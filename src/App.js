da38eae0b1354e288e30b9449cfbd4c85ff2c383
Rishi-Kant <kant3397@gmail.com>
2023-04-11 21:43:06 +0530

    assignment2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,108 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import "./App.css";
+import { useState, useRef } from "react";
+export default function App() {
+  const [data, setData] = useState({ name: "", email: "", password: "" });
+  const [array, setArray] = useState([]);
+  const refernce = useRef();
+  const handleChange = (e) => {
+    e.preventDefault();
+    setArray([...array, data]);
+  };
+  const handleDelete = (ind) => {
+    const newArray = array.filter((ele, index) => {
+      return ind !== index;
+    });
+    setArray(newArray);
+  };
+  const handleUpdate = (ind, e) => {
+    for (let i = 0; i < array.length; i++) {
+      if (ind === i) {
+        setData({ ...data, [e.target.name]: e.target.value });
+        array.splice(ind, 1);
+      }
+    }
+    setArray([...array, data]);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="section1">
+        <form className="forms" onSubmit={handleChange}>
+          <input
+            type="text"
+            placeholder="Name.."
+            onChange={(e) => {
+              e.preventDefault();
+              setData({ ...data, name: e.target.value });
+            }}
+          />
+          <input
+            type="email"
+            placeholder="Email"
+            onChange={(e) => {
+              e.preventDefault();
+              setData({ ...data, email: e.target.value });
+            }}
+          />
+          <input
+            type="password"
+            placeholder="Password.."
+            onChange={(e) => {
+              e.preventDefault();
+              setData({ ...data, password: e.target.value });
+            }}
+          />
+          <button
+            onClick={(e) => {
+              handleChange(e);
+            }}
+          >
+            Submit
+          </button>
+        </form>
+        <div className="sec2">
+          <span>
+            Hello My name is {data.name} and email is {data.email} and password
+            is {data.password}
+          </span>
+        </div>
+      </div>
+      <div className="section2">
+        <table>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Password</th>
+              <th colSpan="2">Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            {array.map((ele, ind) => {
+              return (
+                <tr key={ind} ref={refernce}>
+                  <td>{ele.name}</td>
+                  <td>{ele.email}</td>
+                  <td>{ele.password}</td>
+                  <td
+                    onClick={() => {
+                      handleDelete(ind);
+                    }}
+                  >
+                    Delete
+                  </td>
+                  <td
+                    onClick={(e) => {
+                      handleUpdate(ind, e);
+                    }}
+                  >
+                    Update
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
-
-export default App;
